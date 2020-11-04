089b84bac8686543d854df7a760f04f464d153a5
Xiaofeng Wang <xfun68@gmail.com>
2020-11-04 12:24:43 +0800

    add gtb comments

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
@@ -3,6 +3,8 @@ package com.thoughtworks.capability.gtb.entrancequiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//TODO GTB-综合: * 整体完成度太低，暂时无法给出更多评价
+
 @SpringBootApplication
 public class GtbEntranceQuizApplication {
 
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/MemberController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/MemberController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//TODO GTB-完成度: * 没有任何 API 实现
 @RestController
 public class MemberController {
     @Autowired
